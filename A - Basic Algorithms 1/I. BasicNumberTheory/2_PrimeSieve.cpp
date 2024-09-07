e0ac9d307635732736be0f258bab2e7292dc63d1
Tcoder206 <proglang704206@gmail.com>
2024-09-07 23:27:01 +0700

    Bump

--- A - Basic Algorithms 1/I. BasicNumberTheory/2_PrimeSieve.cpp
+++ A - Basic Algorithms 1/I. BasicNumberTheory/2_PrimeSieve.cpp
@@ -1,5 +1,7 @@
 /*
 
+Nháp: (check 1 số n bất kỳ xem có phải số nguyên tố)
+
 Nếu n là hợp số: n = a * b (a hoặc b hoặc cả 2 số phải nhỏ hơn n)
 Giả sử: a <= b
 <->     a * a <= a * b (Nhân 2 vế vs a)
@@ -15,25 +17,26 @@ Giả sử: a <= b
 #include<bits/stdc++.h>
 using namespace std;
 
-// Check xem có phải số nguyên tố ko?
-int isPrime(int n) {
-    if(n < 2) return false; // 0 và 1 ko phải là 1 số nguyên tố
-    for(int i = 2; i * i <= n; i++) { // Check số nguyên tố bắt đầu từ số 2
-        if(n % i == 0) return false; // Nếu chia hết cho số i khác ngoài 1 và n thì ko phải số nguyên tố
-    }
-    return true;
-}
+// // Check xem có phải số nguyên tố ko?
+// int isPrime(int n) {
+//     if(n < 2) return false; // 0 và 1 ko phải là 1 số nguyên tố
+//     for(int i = 2; i * i <= n; i++) { // Check số nguyên tố bắt đầu từ số 2
+//         if(n % i == 0) return false; // Nếu chia hết cho số i khác ngoài 1 và n thì ko phải số nguyên tố
+//     }
+//     return true;
+// }
 
 // In nguyên tố bằng sàng Eratosthenes
 void sieve(int n) { // n là con số giới hạn sẽ dừng lại khi sàng số nguyên tố
     int prime[n + 1]; // Tạo một mảng lưu trữ các số nguyên tố
-    for(int i = 0; i <= n; i++) {
+    for(int i = 0; i <= n; i++) { // Nhỏ hơn bằng n do prime có số lượng là n + 1
         prime[i] = 1; // i sẽ là số nguyên tố nếu giá trị là 1, nếu ko phải thì là 0
     }
     prime[0] = prime[1] = 0; // 0 và 1 ko phải số nguyên tố nên loại
     for(int i = 2; i <= n; i++) { // Đã loại số 0 và 1 nên xét từ số 2
         if(prime[i]) { // Check nếu như nó là số nguyên tố
             for(int j = i * i; j <= n; j += i) { // Lọc những số chia hết cho i nên bước nhảy phải cộng thêm i
+            // Bắt đầu từ i * i vì các số nhỏ hơn i * i đã được đánh dấu trước là số nguyên tố
                 prime[j] = 0;
             }
         }
