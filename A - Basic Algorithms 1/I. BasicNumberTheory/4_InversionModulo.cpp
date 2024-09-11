f43858084f139a9b1f3be293e3c4bef9de170fcb
Tcoder206 <proglang704206@gmail.com>
2024-09-11 23:50:53 +0700

    Bump

--- A - Basic Algorithms 1/I. BasicNumberTheory/4_InversionModulo.cpp
+++ A - Basic Algorithms 1/I. BasicNumberTheory/4_InversionModulo.cpp
@@ -7,5 +7,6 @@ VD:
 6^-1 mod 11 = 2 (Do 2*6 mod 11 = 1)
 13^-1 mod 15 = 7 (Do 7*13 mod 15 = 1)
 
+https://www.youtube.com/watch?v=oFtVYP9sDxE&ab_channel=%C4%90%C3%B4%CC%83V%C4%83n%C4%90%C6%B0%CC%81c-%C4%90HGTVT
 
 */
--- C - Greedy Algorithms/6_bai_toan_doi_tien.cpp
+++ C - Greedy Algorithms/6_bai_toan_doi_tien.cpp
@@ -3,7 +3,8 @@
 https://www.youtube.com/watch?v=1D-BC82FdZM
 
 Tại ngân hàng có các mệnh giá bằng 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000,
-số lượng tờ tiền mỗi mệnh giá là ko hạn chế. Mỗi người cần đổi số tiền có giá trị bằng N.
+số lượng tờ tiền mỗi mệnh giá là ko hạn chế.
+Mỗi người cần đổi số tiền có giá trị bằng N.
 Hãy xác định xem số tờ tiền ít nhất sau khi đổi là bao nhiêu?
 
 Input: 
@@ -20,4 +21,22 @@ Input   Output
 121
 
 */
+Code đang bị lỗi
+#include<iostream>
+using namespace std;
+int main() {
+    int n = 2;
+    int input[n] = {70, 121};
+    for(int i = 0; i < n; i++) {
 
+        int a[10] = {1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+        int count = 0;
+        for(int j = 9; j >= 0; j--){
+            count += input[n]/a[j];
+            input[n] %= a[j];
+        }
+        cout << count << " ";
+
+    }
+    return 0;
+}
