3f0aa3fd554a75fb6604cf95e1eb776592ed60da
Tcoder206 <proglang704206@gmail.com>
2024-09-23 23:50:37 +0700

    Update Code Subset Sum DP

--- D - Basic Algorithms 2/II. Dynamic Programming/06_subset_sum.cpp
+++ D - Basic Algorithms 2/II. Dynamic Programming/06_subset_sum.cpp
@@ -1,4 +1,37 @@
+/*
+8 92
+69 16 82 170 31 24 45 112
+true
+
+4 8
+1 2 5 7
+true
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+bool SubsetSum(int n, int sum, int *a) {
+    bool dp[n + 1][sum + 1];
+
+    for(int i = 0; i < n; i++) dp[i][0] = true; // Toàn bộ cột j = 0 sẽ là true
+    for(int j = 1; j < sum; j++) dp[0][j] = false; // j = 0 đã xét trong vòng lặp bên trên
+
+    for(int i = 1; i <= n; i++) { // i = 0 và j = 0 trong biến dp[][] đã đc xử lý
+        for(int j = 1; j <= sum; j++) {
+            if(a[i - 1] > j){ // Index của mảng a bắt đầu từ 0
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - a[i]];
+            }
+        }
+    }
+    return dp[n][sum];
+}
 int main() {
-      
+    int n, sum; cin >> n >> sum;
+    int a[n];
+    for(int i = 0; i < n; i++) cin >> a[i];
+    cout << boolalpha << SubsetSum(n, sum, a);
     return 0;
 }
